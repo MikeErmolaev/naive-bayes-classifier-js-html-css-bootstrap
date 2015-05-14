90e58dd61f3ec1e9fd6eb2a45da9436fbde858f4
Mike <spark4304@gmail.com>
2015-05-14 08:07:57 +0300

    Implemented functionality of Classifier

--- app/scripts/classifier.js
+++ app/scripts/classifier.js
@@ -1,11 +1,103 @@
 'use strict';
-function Classifier(){
-	this.vocabulary=[];
-	this.trainingSet={};
+
+function Classifier() {
+	this.vocabulary = [];
+	this.trainingSet = {};
+	this.docCount = 0;
 }
-Classifier.prototype.train = function(group,features){
+Classifier.prototype.train = function(group, features) {
+	var featuresArray,
+		that = this;
+	if (!group || !features) {
+		throw new Error("need some data to work")
+	}
+	if (typeof group != 'string' || typeof features != 'string') {
+		throw new Error('wrong input!');
+	}
+	featuresArray = makeBagOfWords(features);
+	if (Object.keys(that.trainingSet).indexOf(group) > -1) {
+		that.trainingSet[group].concat(featuresArray);
+		that.trainingSet[group].classPrior++;
+	} else {
+		that.trainingSet[group] = featuresArray;
+		that.trainingSet[group].classPrior = 1;
+	}
+	that.docCount++;
+	featuresArray.forEach(function(feature) {
+		if (that.vocabulary.indexOf(feature) == -1) {
+			that.vocabulary.push(feature);
+		}
+	});
 
 };
-Classifier.prototype.classify=function(text){
-
+Classifier.prototype.classify = function(text) {
+	var textArray,
+		that = this;
+	if (!text) {
+		throw new Error("need some data to work")
+	}
+	if (typeof text != 'string') {
+		throw new Error('wrong input!');
+	}
+	textArray = makeBagOfWords(text);
+	if (textArray.every(function(word) {
+			if (that.vocabulary.indexOf(word) == -1) {
+				return true;
+			} else {
+				return false;
+			}
+		})) {
+		return 'unknown category';
+	}
+	Object.keys(that.trainingSet).forEach(function(group) {
+		that.trainingSet[group].probability = 1;
+		textArray.forEach(function(word) {
+			that.trainingSet[group].probability *= (countWordRepetitions(word, that.trainingSet[group]) + 1) / (that.trainingSet[group].length + that.vocabulary.length);
+		});
+		that.trainingSet[group].probability *= that.trainingSet[group].classPrior / that.docCount;
+	});
+	return Object.keys(that.trainingSet).reduce(function(a, b) {
+		if (that.trainingSet[a].probability > that.trainingSet[b].probability) {
+			return a;
+		} else {
+			return b;
+		}
+	});
 };
+Classifier.prototype.getProbabilityToClass = function(group, text) {
+	var textArray,
+		probability=1,
+		that=this;
+	if (!text && !group) {
+		throw new Error("need some data to work")
+	}
+	if (!text) {
+		if (typeof group != 'string') {
+			throw new Error('wrong input!');
+		}
+		return that.trainingSet[group].classPrior / that.docCount;
+	} else {
+		if (typeof group != 'string' || typeof text != 'string') {
+			throw new Error('wrong input!');
+		}
+		textArray = makeBagOfWords(text);
+		textArray.forEach(function(word){
+			probability*=(that.trainingSet[group].classPrior / that.docCount) * (countWordRepetitions(word, that.trainingSet[group]) + 1) / (that.trainingSet[group].length + that.vocabulary.length);
+		});
+		return probability;
+	}
+}
+
+function countWordRepetitions(word, array) {
+	var count = 0;
+	array.forEach(function(item) {
+		if (item === word) {
+			count++;
+		}
+	});
+	return count;
+}
+
+function makeBagOfWords(text) {
+	return text.replace(/[\W\d]/g, ' ').replace(/\s{2,}/g, ' ').trim().toLowerCase().split(' ');
+}
--- tests/test.js
+++ tests/test.js
@@ -1,70 +1,136 @@
 "use strict";
 describe("Classifier", function() {
+	var classifier;
 	beforeEach(function() {
-		var classifier = new Classifier();
+		classifier = new Classifier();
 	});
 	describe("constructor", function() {
-		it("should have vocabulary and trainingSet properties", function() {
-			expect(classifier).to.have.property('vocabulary').with.property('trainingSet');
+
+		it("should have vocabulary, trainingSet and docCount properties", function() {
+			expect(classifier).to.have.property('vocabulary');
+			expect(classifier).to.have.property('trainingSet');
+			expect(classifier).to.have.property('docCount');
 			expect(classifier.trainingSet).to.be.an('object')
+			expect(classifier.docCount).to.be.equal(0);
 		});
 		it("should have empty properties as default at creation", function() {
 			expect(classifier.vocabulary).to.be.empty;
 			expect(classifier.trainingSet).to.be.empty;
+			expect(classifier.docCount).to.be.equal(0);
 			classifier = new Classifier('asdf', 235425, 'fsdf');
 			expect(classifier.vocabulary).to.be.empty;
 			expect(classifier.trainingSet).to.be.empty;
+			expect(classifier.docCount).to.be.equal(0);
 		});
 	});
 	describe("#train", function() {
-		it("should throw error if provided arguments aren't string",function(){
-			expect(function(){
-				classifier.train(1,{asd:'asd'});
-			}).to.throw(Error);
+		it("should throw error if some of arguments aren't provided", function() {
+			expect(function() {
+				classifier.train('a');
+			}).to.throw(Error("need some data to work").message);
+
+			expect(function() {
+				classifier.train();
+			}).to.throw(Error("need some data to work").message);
 		});
-		it("should update vocabulary length by count of unique words",function(){
+		it("should throw error if provided arguments aren't string", function() {
+			expect(function() {
+				classifier.train(1, {
+					asd: 'asd'
+				});
+			}).to.throw(Error("wrong input!").message);
+		});
+		it("should update vocabulary length by count of unique words and docCount by count of new docs", function() {
 			expect(classifier.vocabulary).to.have.length(0);
-			classifier.train("spam","hello buy some viagra");
+			expect(classifier.docCount).to.be.equal(0);
+			classifier.train("spam", "hello buy some viagra");
+			expect(classifier.docCount).to.be.equal(1);
 			expect(classifier.vocabulary).to.have.length(4);
-			classifier.train("not spam","hello morning good day");
+			expect(classifier.docCount).to.be.equal(1);
+			classifier.train("not spam", "hello morning good day");
+			expect(classifier.docCount).to.be.equal(2);
 			expect(classifier.vocabulary).to.have.length(7);
 		});
-		it("should increase trainingSet length when adding new class and add this class as new property to trainingSet",function(){
+		it("should increase trainingSet length when adding new class and add this class as new property to trainingSet", function() {
 			expect(Object.keys(classifier.trainingSet)).to.have.length(0);
-			classifier.train("classA","qwerty asdf hfd");
+			classifier.train("classA", "qwerty asdf hfd");
 			expect(Object.keys(classifier.trainingSet)).to.have.length(1);
-			expect(classifier.trainingSet).to.have.property('classA',['qwerty','asdf','hfd']);
-
+			expect(classifier.trainingSet).to.have.property('classA');
+			expect(JSON.stringify(classifier.trainingSet.classA)).to.be.equal(JSON.stringify(['qwerty', 'asdf', 'hfd']));
 		});
-		it("should add only words to trainingSet",function(){
-			classifier.train("classA","qwerty asdf hfd");
+		it("should add only words to trainingSet", function() {
+			classifier.train("classA", "qwerty asdf hfd");
 			expect(classifier.trainingSet['classA']).to.have.length(3);
-			classifier.train("classB","123 qwerty !@#%,.");
+			classifier.train("classB", "123 qwerty !@#%,.");
 			expect(classifier.trainingSet['classB']).to.have.length(1);
 		});
-	});
-	describe("#classify",function(){
-		before(function(){
-			var res;
-			classifier.train("not spam","hello morning good day");
-			classifier.train("spam","hello buy some viagra");
+		it("should create one megadoc from a lot of docs of one class", function() {
+			classifier.train("classA", "qwerty asdf hfd");
+			expect(Object.keys(classifier.trainingSet)).to.have.length(1);
+			classifier.train("classA", "tyrt we");
+			expect(Object.keys(classifier.trainingSet)).to.have.length(1);
+			classifier.train("classA", "gh s");
+			expect(Object.keys(classifier.trainingSet)).to.have.length(1);
 		});
-		it("should throw error if provided argument isn't string",function(){
-			expect(function(){
-				res=classifier.classify(1);
-			}).to.throw(Error);
-			expect(function(){
-				res=classifier.classify({lol:"lol"});
-			}).to.throw(Error);
-		})
-		it("should decide to which class goes provided text",function(){
-			res=classifier.classify("good day, i am viagra seller. Do you want to buy some?");
+	});
+	describe("#classify", function() {
+		var res;
+		beforeEach(function() {
+			classifier.train("not spam", "hello morning good day");
+			classifier.train("spam", "hello buy some viagra");
+		});
+		it("should throw error if no arguments are provided", function() {
+			expect(function() {
+				res = classifier.classify();
+			}).to.throw(Error("need some data to work").message);
+		});
+		it("should throw error if provided argument isn't string", function() {
+			expect(function() {
+				res = classifier.classify(1);
+			}).to.throw(Error("wrong input").message);
+			expect(function() {
+				res = classifier.classify({
+					lol: "lol"
+				});
+			}).to.throw(Error("wrong input").message);
+		});
+		it("should decide to which class goes provided text", function() {
+			res = classifier.classify("good day, i am viagra seller. Do you want to buy some?");
 			expect(res).to.be.equal("spam");
 		});
-		it("should return 'unknown category'",function(){
-			res=classifier.classify("unknown words here");
+		it("should return 'unknown category'", function() {
+			res = classifier.classify("unknown words here");
 			expect(res).to.be.equal("unknown category");
 		});
+		it("should be able to train further after classification and return correct result", function() {
+			res = classifier.classify("good day, i am viagra seller. Do you want to buy some?");
+			expect(res).to.be.equal("spam");
+			classifier.train("invitaion", "come at pm evening dinner");
+			res = classifier.classify("Good evening! Come to dinner at 6 pm!");
+			expect(res).to.be.equal("invitaion");
+			classifier.train("spam", "porn porn porn buy drugs");
+			res = classifier.classify("Watch porn, eat drugs and buy viagra!");
+			expect(res).to.be.equal("spam");
+		});
 	});
-
+	describe("#getProbabilityToClass", function() {
+		var res;
+		beforeEach(function() {
+			classifier.train("not spam", "hello morning good day");
+			classifier.train("spam", "hello buy some viagra");
+		});
+		it("should throw error if no arguments are provided", function() {
+			expect(function() {
+				classifier.getProbabilityToClass();
+			}).to.throw(Error("need some data to work").message);
+		});
+		it("should return class probability(class prior) if only class is provided",function(){
+			res = classifier.getProbabilityToClass("spam");
+			expect(res).to.be.equal(0.5);
+		});
+		it("should return probability number of provided text to belong to provided class", function() {
+			res = classifier.getProbabilityToClass("spam", "hello good evening buy chips come to me");
+			expect(res).to.be.a('number');
+		});
+	});
 });
